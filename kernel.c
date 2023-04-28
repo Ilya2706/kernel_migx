0798618db398692d9736f3c2eb43c417f8392886
Ilya <85501909+Ilya2706@users.noreply.github.com>
2023-04-28 22:01:03 +0200

    First upload. Kernel 0.0.1
    Only print text

---
+++ kernel.c
@@ -0,0 +1,44 @@
+#define COLOUR 0x03
+char *vidptr = (char*)0xb8000;
+unsigned int i = 0, j = 0;
+
+void clean(){
+	j = 0;
+	i = 0;
+	while(j < 80 * 25 * 2) {
+		vidptr[j] = ' ' ;
+		vidptr[j+1] = COLOUR;
+		j = j + 2;
+	}
+}
+
+void print_video(const char * str){
+	j = 0;
+	// i = 0;
+	while(vidptr[i] = '\0') {
+		i = i + 2;
+	}
+
+	i = i + 2;
+
+	while(str[j] != '\0') {
+		vidptr[i] = str[j];
+		vidptr[i+1] = COLOUR;
+		++j;
+		i = i+2;
+	} 
+}
+
+void kmain(void) {
+
+	clean();
+	
+	const char *str = "my first kernel on c!";
+	const char *str_1 = "Hello world! my kernel!";
+	
+	print_video(str);
+
+	print_video(str_1);
+
+}
+
